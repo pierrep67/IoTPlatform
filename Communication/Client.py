bc9aaee3ebf4dabe1698a2a366a0b49f0c892595
mathieup6 <mathieu.palomares@ecam-strasbourg.eu>
2018-01-14 13:01:48 +0100

    Update code files

--- Communication/Client.py
+++ Communication/Client.py
@@ -4,33 +4,30 @@ from opcua.common.methods import call_method
 from opcua.common.subscription import Subscription
 from opcua.ua import VariantType
 
-import sys
-sys.path.insert(0, "..")
-
 if __name__ == '__main__':
 
+        ## Client creation and connection
         client = Client("opc.tcp://localhost:40840/iotproject/server/")
         client.connect()
         
-        root = client.get_root_node()
+
+        root = client.get_root_node() # The root node is got
         print("Root node is: ", root)
-        print("Children of root are: ", root.get_children())
+        print("Children of root are: ", root.get_children()) # The root node children are got
         objects = client.get_objects_node()
-        print("Objects node is: ", objects)
+        print("Objects node is: ", objects) #Object nodes are listed
         
-        # Now getting a variable node using its browse path
+        # Now a variable is got using its browse path
         myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyStringVariable"])
+        print("myvar is: ", myvar) 
         
-        print("myvar is: ", myvar)
+        # A string variable data value is got
         mystrg = myvar.get_data_value()
         print("Mystrg is :",mystrg)
         
+        # A variable value is got, modified and checked
         myval = myvar.get_value()
         print("My value is :",myval)
-        
         myvar.set_value("Good bye !")
-        
         myval = myvar.get_value()
         print("My value is :",myval)
-        
-        print("node :",client.get_node(ua.NodeId(1003, 2)))
--- Communication/Server.py
+++ Communication/Server.py
@@ -1,43 +1,32 @@
 import sys
-sys.path.insert(0, "..")
 import time
-import sys
-sys.path.insert(0, "..")
-
 from opcua import ua, Server
 
 if __name__ == "__main__":
 
-    # setup our server
+    ## The server is set up
     server = Server()
-    server.set_endpoint("opc.tcp://localhost:40840/iotproject/server/")
+    server.set_endpoint("opc.tcp://localhost:40840/iotproject/server/") #endpoint url : where the webservice can be accessed by a client application
     
-    # setup our own namespace, not really necessary but should as spec
-    uri = "http://iotproject.communication"
-    idx = server.register_namespace(uri)
+    uri = "http://iotproject.communication"  #"URI : a Uniform Resource Identifier (URI) is a string of characters used to identify a resource."
+    idx = server.register_namespace(uri) #"namespace : set of symbols that are used to organize objects of various kinds, so that these objects may be referred to by name"
 
-    # get Objects node, this is where we should put our nodes
-    objects = server.get_objects_node()
+    objects = server.get_objects_node() #this is where we should put our nodes
 
-    # populating our address space
-    myobj = objects.add_object(idx, "MyObject")
-    myvar = myobj.add_variable(idx, "MyVariable", 1.0)
-    myvar.set_writable()   #writable by clients
-    mystring  = myobj.add_variable(idx, "MyStringVariable", "Hello world !")
-    mystring.set_writable()
+    myobj = objects.add_object(idx, "MyObject") #Object creation
+    myvar = myobj.add_variable(idx, "MyVariable", 1.0) #Variable creation
+    myvar.set_writable()  #The previous variable is defined as writable
+    mystring  = myobj.add_variable(idx, "MyStringVariable", "Hello world !") #A string variable is added to the object "myobj"
+    mystring.set_writable() #The string variable is defined as writable
 
-    # starting!
+    ## The server is started
     server.start()
     print("Server started !")
     
-    #The server is running
+    ##The server is running
     try:
-        count = 0
         while True:
             time.sleep(1)
-            count += 0.1
-            myvar.set_value(count)
-            
     finally:
         server.stop()
         print('Server stopped !')
